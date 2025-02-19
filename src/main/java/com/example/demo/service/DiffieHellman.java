33101e700bfeca8032f4adecc71b2a1bb797980d
Supatz317 <supatsorn.su@kkumail.com>
2025-02-19 16:30:32 +0700

    add method getter

--- src/main/java/com/example/demo/service/DiffieHellman.java
+++ src/main/java/com/example/demo/service/DiffieHellman.java
@@ -3,9 +3,14 @@ package com.example.demo.service;
 
 import java.math.BigInteger;
 
+import com.example.demo.model.Client;
+
 
 public interface  DiffieHellman {
-    public BigInteger generatePublicKey(BigInteger base, BigInteger modulus, BigInteger clientKey);
-    public String encrypt(String message);
-    public String decrypt(String message);
+    public BigInteger generatePublicKey(Client client);
+    public void generateSecretKey(Client client, BigInteger genKey);
+    public String encrypt(Client client, String message);
+    public String decrypt(Client client, String message);
+    public BigInteger getP();
+    public BigInteger getG();
 }
